07e724dadef4c65d91ee17f62d7eeab000a5153b
JOHN ADAN J R <johnadan0606@gmail.com>
2026-02-28 20:37:33 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "lehe-logs-v10";
+
+const CORE_ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+
+  "./css/style.css",
+
+  "./js/app.js",
+  "./js/storage.js",
+
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(k => k !== CACHE_NAME)
+          .map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
